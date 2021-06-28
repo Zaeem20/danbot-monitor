b4e226aa28bc60243f66e51c77576c3d83519c94
Zaeem Technical <duranizaeem@gmail.com>
2021-06-29 03:51:58 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,21 @@
+# Auto Setup Script
+import json
+import time
+import os
+
+print("033\[1;92m")
+
+print("Auto Setup Script")
+print 
+time.sleep(2)
+apikey = input("Enter API key:- ")
+bsid = input("Enter BSID")
+psid = input("Enter PSID")
+
+try:
+  with open("config.json", "w") as a:
+    json.dumps("apikey" , apikey)
+    json.dumps("bsid" , bsid)
+    json.dumps("psid" , psid)
+except:
+  print("Operation Failed")
